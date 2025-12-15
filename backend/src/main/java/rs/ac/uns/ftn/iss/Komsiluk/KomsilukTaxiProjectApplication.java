5c4f62077138cb199c5d18841f5d08ae4e364b6f
UrosMilinovic <151851654+UrosMilinovic@users.noreply.github.com>
2025-12-15 18:09:48 +0100

    Add Spring Boot backend project

---
+++ backend/src/main/java/rs/ac/uns/ftn/iss/Komsiluk/KomsilukTaxiProjectApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.iss.Komsiluk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomsilukTaxiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomsilukTaxiProjectApplication.class, args);
+	}
+
+}
