e6a13268776ade980620f39d45ee99173c28f65b
Nikola Savić <nikolasa004@gmail.com>
2025-12-16 01:36:31 +0100

    feat: Add user profile page panel design
    Introduces a new user profile page with sidebar and details components. Updates routing to include the profile page and redirects the root path to it for testing. Cleans up placeholder content in app.html, and removes unused files.

--- frontend/komsiluk-taxiProject/src/app/app.routes.ts
+++ frontend/komsiluk-taxiProject/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserProfilePageComponent } from './features/profile/pages/user-profile-page/user-profile-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'profile', component: UserProfilePageComponent },
+
+  // temporary redirect to profile for easy testing
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+];
