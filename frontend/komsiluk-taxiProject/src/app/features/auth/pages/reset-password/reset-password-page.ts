0a483150c305d5ec32a95dbfd5fd61875217383b
Branislav Markovic <banemarkovic2004@gmail.com>
2025-12-17 18:28:36 +0100

    feat: Created file structure required for login feature

---
+++ frontend/komsiluk-taxiProject/src/app/features/auth/pages/reset-password/reset-password-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-page',
+  imports: [],
+  templateUrl: './reset-password-page.html',
+  styleUrl: './reset-password-page.css',
+})
+export class ResetPasswordPage {
+
+}
---
+++ frontend/komsiluk-taxiProject/src/app/features/auth/pages/verify-code/verify-code-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-code-page',
+  imports: [],
+  templateUrl: './verify-code-page.html',
+  styleUrl: './verify-code-page.css',
+})
+export class VerifyCodePage {
+
+}
