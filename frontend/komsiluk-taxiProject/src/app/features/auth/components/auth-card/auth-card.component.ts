bd73f7774bb1c8dba387f42e29d64065d6132bb5
Branislav Markovic <banemarkovic2004@gmail.com>
2025-12-18 00:25:56 +0100

    feat: Add auth window template, add Angular Material, Create Login page

--- frontend/komsiluk-taxiProject/src/app/features/auth/components/auth-card/auth-card.component.ts
+++ frontend/komsiluk-taxiProject/src/app/features/auth/components/auth-card/auth-card.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-card',
-  imports: [],
+  standalone: true,
   templateUrl: './auth-card.component.html',
   styleUrl: './auth-card.component.css',
 })
-export class AuthCard {
-
+export class AuthCardComponent {
+  @Input() title!: string;
+  @Input() buttonText = 'Continue';
 }
--- frontend/komsiluk-taxiProject/src/app/features/auth/pages/login/login-page.component.ts
+++ frontend/komsiluk-taxiProject/src/app/features/auth/pages/login/login-page.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthCardComponent } from '../../components/auth-card/auth-card.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {FormControl, FormGroup, Validators, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [AuthCardComponent,MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatButtonModule,CommonModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
 export class LoginPage {
+  LoginForm: FormGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
 
+    onSubmit() {
+      if (this.LoginForm.invalid) {
+        this.LoginForm.markAllAsTouched();
+        return;
+      }
+
+      console.log(this.LoginForm.value);
+    }
 }
