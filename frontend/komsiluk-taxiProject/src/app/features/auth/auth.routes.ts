d964eacf581066b948e5927d61b603116c85a96b
Branislav Markovic <banemarkovic2004@gmail.com>
2025-12-23 23:16:54 +0100

    Complete successful registration component

--- frontend/komsiluk-taxiProject/src/app/features/auth/auth.routes.ts
+++ frontend/komsiluk-taxiProject/src/app/features/auth/auth.routes.ts
@@ -4,11 +4,13 @@ import { ResetPasswordComponent } from './pages/reset-password/reset-password-pa
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { ActivationMessageComponent } from './pages/activation-message/activation-message.component';
 import { RiderRegistrationComponent } from './pages/rider-registration/rider-registration.component';
+import { SuccessfulRegistrationComponent } from './pages/successful-registration/successful-registration.component';
 
 export const AUTH_ROUTES: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'activation-message', component: ActivationMessageComponent },
-  { path: 'register-rider', component: RiderRegistrationComponent }
+  { path: 'register-rider', component: RiderRegistrationComponent },
+  { path: 'successful-registration', component: SuccessfulRegistrationComponent }
 ];
