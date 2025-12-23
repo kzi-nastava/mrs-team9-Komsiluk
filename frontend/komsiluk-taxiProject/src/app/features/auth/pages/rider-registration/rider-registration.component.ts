45aaa1b2ba47dee2fb3f489a12d9107569028733
Branislav Markovic <banemarkovic2004@gmail.com>
2025-12-23 21:26:42 +0100

    Add rider-registration component

---
+++ frontend/komsiluk-taxiProject/src/app/features/auth/pages/rider-registration/rider-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rider-registration.component',
+  imports: [],
+  templateUrl: './rider-registration.component.html',
+  styleUrl: './rider-registration.component.css',
+})
+export class RiderRegistrationComponent {
+
+}
