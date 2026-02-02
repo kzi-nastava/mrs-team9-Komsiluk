5e5677e2ddb61a7f8550eab2aab3d737a8118461
Branislav Markovic <banemarkovic2004@gmail.com>
2026-02-02 16:08:46 +0100

    minor changes in relation to handling UserDetails username not found error in spring security and passenger history

--- backend/src/main/java/rs/ac/uns/ftn/iss/Komsiluk/services/exceptions/UnauthorizedAccessException.java
+++ backend/src/main/java/rs/ac/uns/ftn/iss/Komsiluk/services/exceptions/UnauthorizedAccessException.java
@@ -1,4 +1,17 @@
 package rs.ac.uns.ftn.iss.Komsiluk.services.exceptions;
 
-public class UnauthorizedAccessException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "Unauthorized access")
+public class UnauthorizedAccessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public UnauthorizedAccessException() {
+        super();
+    }
+
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
 }
